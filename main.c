4bf60d3613135c3fb6452eec019310f5493a0a20
Abdullah <abdullahkhan.ie.official@gmail.com>
2023-08-25 15:29:08 +0600

    made it more readable

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ struct ticket
     int code;
 };
 int home_screen();
+void default_movie_list(struct ticket movies[]);
 void about_us();
 // int admin();
 void construction();
@@ -16,49 +17,19 @@ void customer_mode();
 void movie_list_print(struct ticket movies[]);
 int ticket_booking(struct ticket movies[], int *ticket);
 float payment_checkout(float cost);
+void admin_mode();
 int main()
 {
-    int total_movie = 9;
+    int total_movie = 10;
     struct ticket movies[N];
-    strcpy(movies[0].name, "Dhaka Attack             101");
-    movies[0].price = 350;
-    movies[0].code = 101;
-    strcpy(movies[1].name, "Mission Extreme          102");
-    movies[1].price = 350;
-    movies[1].code = 102;
-    strcpy(movies[2].name, "Din: The Day              103");
-    movies[2].price = 350;
-    movies[2].code = 103;
-    strcpy(movies[3].name, "American Psycho          201");
-    movies[3].price = 350;
-    movies[3].code = 201;
-    strcpy(movies[4].name, "Oppenheimer              202");
-    movies[4].price = 350;
-    movies[4].code = 202;
-    strcpy(movies[5].name, "The Dark Knight          203");
-    movies[5].price = 350;
-    movies[5].code = 203;
-    strcpy(movies[6].name, "Barbie                   204");
-    movies[6].price = 350;
-    movies[6].code = 204;
-    strcpy(movies[7].name, "Avengers: End Game       205");
-    movies[7].price = 350;
-    movies[7].code = 205;
-    strcpy(movies[8].name, "Pathan                   301");
-    movies[8].price = 350;
-    movies[8].code = 301;
-    strcpy(movies[9].name, "Vikram Vedha             302");
-    movies[9].price = 350;
-    movies[9].code = 302;
 home:
-
+    default_movie_list(movies);
     int home_menu = home_screen();
     float total_cost = 0;
     if (home_menu == 1)
     {
-
-        construction();
-        if (return_home(3) == 1)
+        admin_mode();
+        if (return_home(1) == 1)
         {
             goto home;
         }
@@ -72,20 +43,20 @@ home:
         switch (option)
         {
         case 1:
-            construction();
-            if (return_home(3) == 1)
-            {
-                goto home;
-            }
-            break;
-        case 2:
-            movie_list_print(movies);
+            customer_mode();
             if (return_home(1) == 1)
             {
                 goto home;
             }
             break;
         case 3:
+            movie_list_print(movies);
+            if (return_home(1) == 1)
+            {
+                goto home;
+            }
+            break;
+        case 4:
             int ticket_code = 0;
             if (ticket_booking(movies, &ticket_code) != 0)
             {
@@ -97,12 +68,15 @@ home:
                 goto home;
             }
             break;
-        case 4:
+        case 5:
             construction();
             if (return_home(1) == 1)
             {
                 goto home;
             }
+            break;
+        case 6:
+            goto home;
             break;
         default:
             printf("\t\tInvalid Input\n");
@@ -151,6 +125,39 @@ int home_screen()
     }
     return x;
 }
+void default_movie_list(struct ticket movies[])
+{
+    strcpy(movies[0].name, "Dhaka Attack             101");
+    movies[0].price = 350;
+    movies[0].code = 101;
+    strcpy(movies[1].name, "Mission Extreme          102");
+    movies[1].price = 350;
+    movies[1].code = 102;
+    strcpy(movies[2].name, "Din: The Day              103");
+    movies[2].price = 350;
+    movies[2].code = 103;
+    strcpy(movies[3].name, "American Psycho          201");
+    movies[3].price = 350;
+    movies[3].code = 201;
+    strcpy(movies[4].name, "Oppenheimer              202");
+    movies[4].price = 350;
+    movies[4].code = 202;
+    strcpy(movies[5].name, "The Dark Knight          203");
+    movies[5].price = 350;
+    movies[5].code = 203;
+    strcpy(movies[6].name, "Barbie                   204");
+    movies[6].price = 350;
+    movies[6].code = 204;
+    strcpy(movies[7].name, "Avengers: End Game       205");
+    movies[7].price = 350;
+    movies[7].code = 205;
+    strcpy(movies[8].name, "Pathan                   301");
+    movies[8].price = 350;
+    movies[8].code = 301;
+    strcpy(movies[9].name, "Vikram Vedha             302");
+    movies[9].price = 350;
+    movies[9].code = 302;
+}
 void about_us()
 {
     printf("\n\n\t\t\t\t\t    |      About Developers       |\t\n");
@@ -183,9 +190,11 @@ void customer_mode()
     printf("\t\t\t    |      CUSTOMER MODE     |\t\n");
     printf("\t\t\t     ------------------------\t\n");
     printf("\n\t\t1 ) Login as Customer\n");
-    printf("\t\t2 ) Movie Information\n");
-    printf("\t\t3 ) Book Tickets\n");
-    printf("\t\t4 ) Logout\n\n");
+    printf("\t\t2 ) Sign Up as Customer\n");
+    printf("\t\t3 ) Movie Information\n");
+    printf("\t\t4 ) Book Tickets\n");
+    printf("\t\t5 ) Logout\n\n");
+    printf("\t\t6 ) Return to Home Menu\n");
 }
 void movie_list_print(struct ticket movies[])
 {
@@ -213,6 +222,7 @@ int ticket_booking(struct ticket movies[], int *code)
             return *code;
         }
     }
+
     printf("\t\tInvalid Input, Returning to Home Menu\n");
     return 0;
 }
@@ -238,4 +248,38 @@ ticket:
         goto ticket;
     }
     return cost;
+}
+void admin_mode()
+{
+    int option;
+    printf("\t\t\t     ------------------------\t\n");
+    printf("\t\t\t    |      ADMIN PANEL       |\t\n");
+    printf("\t\t\t     ------------------------\t\n");
+    printf("\n\t\t\t     Welcome to Admin Panel!!!\n\n\n");
+    printf("\t\t\t\t 1 ) Login\n\n");
+    printf("\t\t\t\t 2 ) Signup\n\n");
+    printf("\t\t\t\t 3 ) Add Movies\n\n");
+    printf("\t\t\t\t 4 ) Delete Movies\n\n");
+    printf("\t\t\t\t 5 ) Revenue Report\n\n");
+    printf("\t\t\t         6 ) Back to Home page\n");
+    printf("\n\t\t\t        Choose your Option \n\t\t\t             ==>  ");
+    scanf("%d", &option);
+    if (option == 1 || option == 2)
+    {
+        construction();
+    }
+    else if (option == 3 || option == 4 || option == 5)
+    {
+        printf("\t\tAccess Denied!\n\t\tReturning To Home Menu\n");
+    }
+
+    else if (option == 6)
+    {
+        printf("n\t\tReturning To Home Menu\n");
+    }
+    else
+    {
+        printf("\t\t\tInvalid Input, Returning To Home Menu\n");
+        return;
+    }
 }
